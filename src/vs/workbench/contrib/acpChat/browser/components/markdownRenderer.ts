ecbb9fbb43e015d74acb8da6f784c922745b36e4
Thomas Wood <odell.wood@gmail.com>
2026-06-07 01:49:32 -0400

    adding katex extension to marked

--- src/vs/workbench/contrib/acpChat/browser/components/markdownRenderer.ts
+++ src/vs/workbench/contrib/acpChat/browser/components/markdownRenderer.ts
@@ -1,4 +1,8 @@
 import { marked } from 'marked';
+import markedKatex from 'marked-katex-extension';
+import 'katex/dist/katex.min.css';
+
+marked.use(markedKatex({ throwOnError: false }));
 
 export function renderMarkdown(text: string): string {
 	return marked.parse(text, { async: false }) as string;
